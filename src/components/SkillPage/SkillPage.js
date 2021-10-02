28b590953b9fbffecb0925284708a2c4a69d4aa6
Romini's coding <74459925+dalping@users.noreply.github.com>
2021-10-03 03:05:12 +0900

    font awesome

--- src/components/SkillPage/SkillPage.js
+++ src/components/SkillPage/SkillPage.js
@@ -1,12 +1,32 @@
 import React from 'react'
 import './SkillPage.css'
+import {
+    Html5Filled
+} from '@ant-design/icons'
+import { faCss3Alt,faHtml5,faJsSquare,faReact} from "@fortawesome/free-brands-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 function SkillPage() {
     return (
         <div className="skillPage">
             <h1>Skills</h1>
-            <div>
-
+            <div className="skillList">
+                <div>
+                    <span className="title"><FontAwesomeIcon icon={faHtml5}/> HTML</span> 
+                    <span className="content"></span>
+                </div>
+                <div>
+                    <span className="title"><FontAwesomeIcon icon={faCss3Alt}/> CSS</span> 
+                    <span className="content">반응형 레이아웃, Styled Component</span>
+                </div>
+                <div>
+                    <span className="title"><FontAwesomeIcon icon={faJsSquare}/> JavaScript</span> 
+                    <span className="content">비동기</span>
+                </div>
+                <div>
+                    <span className="title"><FontAwesomeIcon icon={faReact}/> React.js</span> 
+                    <span className="content">Hooks, Redux, Context API</span>
+                </div>
             </div>
         </div>
     )
